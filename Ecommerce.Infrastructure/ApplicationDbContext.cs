10abd9530585a7590505159ca11a35cca4d1d99a
sayyedulawwab <38081373+sayyedulawwab@users.noreply.github.com>
2024-01-31 23:44:34 +0600

    remove using

--- Ecommerce.Infrastructure/ApplicationDbContext.cs
+++ Ecommerce.Infrastructure/ApplicationDbContext.cs
@@ -2,7 +2,6 @@
 using Ecommerce.Domain.Abstractions;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using System.Data;
 
 namespace Ecommerce.Infrastructure;
