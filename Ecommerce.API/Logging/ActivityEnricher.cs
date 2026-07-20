9d731819250b48a2af795cb08d438c593e399253
Muhammad Sayyedul Awwab <38081373+sayyedulawwab@users.noreply.github.com>
2026-07-20 22:59:57 +0600

    add logs, metrics and traces setup

---
+++ Ecommerce.API/Logging/ActivityEnricher.cs
@@ -0,0 +1,24 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+using System.Diagnostics;
+
+namespace Ecommerce.API.Logging;
+
+public class ActivityEnricher : ILogEventEnricher
+{
+    public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+    {
+        Activity? activity = Activity.Current;
+
+        if (activity == null)
+        {
+            return;
+        }
+
+        logEvent.AddPropertyIfAbsent(
+            propertyFactory.CreateProperty("trace_id", activity.TraceId.ToHexString()));
+
+        logEvent.AddPropertyIfAbsent(
+            propertyFactory.CreateProperty("span_id", activity.SpanId.ToHexString()));
+    }
+}
--- Ecommerce.API/Program.cs
+++ Ecommerce.API/Program.cs
@@ -1,16 +1,86 @@
 using Asp.Versioning;
 using Ecommerce.API;
 using Ecommerce.API.Extensions;
+using Ecommerce.API.Logging;
 using Ecommerce.Application;
 using Ecommerce.Infrastructure;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 using Serilog;
+using System.Diagnostics;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-builder.Host.UseSerilog((context, loggerConfig) =>
-            loggerConfig.ReadFrom.Configuration(context.Configuration));
+builder.Logging.ClearProviders();
 
-builder.Services.AddApplication()
+Activity.DefaultIdFormat = ActivityIdFormat.W3C;
+Activity.ForceDefaultIdFormat = true;
+
+builder.Host.UseSerilog((context, loggerConfigruration) => loggerConfigruration.ReadFrom.Configuration(builder.Configuration)
+.Enrich.With<ActivityEnricher>(), writeToProviders: true);
+
+string? otlpLogsEndpoint = builder.Configuration["Otlp:LogsEndpoint"];
+string? otlpTracesEndpoint = builder.Configuration["Otlp:TracesEndpoint"];
+string? otlpMetricsEndpoint = builder.Configuration["Otlp:MetricsEndpoint"];
+
+
+builder.Services.AddOpenTelemetry()
+    .ConfigureResource(resource => resource.AddService(builder.Environment.ApplicationName).AddAttributes(new Dictionary<string, object>
+    {
+        ["environment.name"] = builder.Environment.EnvironmentName,
+        ["service.name"] = builder.Environment.ApplicationName
+    }))
+    .WithLogging(logging =>
+    {
+        if (!string.IsNullOrWhiteSpace(otlpLogsEndpoint) &&
+            Uri.TryCreate(otlpLogsEndpoint, UriKind.Absolute, out Uri logsUri))
+        {
+            logging.AddOtlpExporter(exporterOptions =>
+            {
+                exporterOptions.Endpoint = logsUri;
+                exporterOptions.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.HttpProtobuf;
+            });
+        }
+    })
+    .WithTracing(tracing =>
+    {
+        tracing.AddSource(builder.Environment.ApplicationName);
+        tracing.AddAspNetCoreInstrumentation();
+        tracing.AddHttpClientInstrumentation();
+
+        if (!string.IsNullOrWhiteSpace(otlpTracesEndpoint) &&
+            Uri.TryCreate(otlpTracesEndpoint, UriKind.Absolute, out Uri tracesUri))
+        {
+            tracing.AddOtlpExporter(exporterOptions =>
+            {
+                exporterOptions.Endpoint = tracesUri;
+                exporterOptions.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.HttpProtobuf;
+            });
+        }
+    })
+    .WithMetrics(metrics =>
+    {
+        metrics.AddAspNetCoreInstrumentation();
+        metrics.AddHttpClientInstrumentation();
+        metrics.AddRuntimeInstrumentation();
+
+        if (!string.IsNullOrWhiteSpace(otlpMetricsEndpoint) &&
+            Uri.TryCreate(otlpMetricsEndpoint, UriKind.Absolute, out Uri metricsUri))
+        {
+            metrics.AddOtlpExporter((exporterOptions, metricReaderOptions) =>
+            {
+                exporterOptions.Endpoint = metricsUri;
+                exporterOptions.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.HttpProtobuf;
+                metricReaderOptions.PeriodicExportingMetricReaderOptions.ExportIntervalMilliseconds = 60000; // 60 seconds
+            });
+        }
+    });
+
+
+builder.Services
+    .AddApplication()
     .AddPresentation()
     .AddInfrastructure(builder.Configuration);
 
